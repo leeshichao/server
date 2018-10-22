e1ae0a1acc9e199753346663681385c9baf39bda
Julius Härtl <jus@bitgrid.net>
2018-10-22 14:58:16 +0200

    Add javascript API for collaboration resources
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- core/src/OCP/index.js
+++ core/src/OCP/index.js
@@ -2,10 +2,12 @@
  *
  */
 import loader from './loader'
+import collaboration from './collaboration'
 
 /** @namespace OCP */
 const OCP = {
 	Loader: loader,
+	Collaboration: collaboration
 };
 
 window['OCP'] = Object.assign({}, window.OCP, OCP)
