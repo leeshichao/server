09083f5d2fe7c79966765f7cdcdb7b2294530c55
Roeland Jago Douma <roeland@famdouma.nl>
2019-01-24 19:58:39 +0100

    Add initial state parsing to OCP
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- core/src/OCP/index.js
+++ core/src/OCP/index.js
@@ -2,10 +2,12 @@
  *
  */
 import loader from './loader'
+import initialState from './initialstate'
 
 /** @namespace OCP */
 const OCP = {
 	Loader: loader,
+	InitialState: initialState,
 };
 
 window['OCP'] = Object.assign({}, window.OCP, OCP)
